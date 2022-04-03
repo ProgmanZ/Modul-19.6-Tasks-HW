34f8bc17fbc8954f3b6dd4a5602fe086b0f6ebc8
ProgmanZ <dumphill@gmail.com>
2022-04-03 17:41:40 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - функции поиска - Готово. Ошибка в методе pop()

--- Task-03.py
+++ Task-03.py
@@ -78,18 +78,26 @@ list_user_vol = list()
 print('Все ключи словаря:', find_keys(data, list_user_key))
 print('Все значения словаря:', find_vol(data, list_user_vol))
 
+# В “ETH” добавить ключ “total_diff” со значением 100.
 data['ETH']['total_diff'] = '100'
 print(data)
+
+# Внутри “fst_token_info” значение ключа “name” поменять с “fdf” на “doge”.
 data['tokens'][0]['fst_token_info']['name'] = 'doge'
 print(data)
-#
+
+# Удалить “total_out” из tokens и присвоить его значение в “total_out” внутри “ETH”.
+data['ETH']['totalOut'] = data['tokens'][0]['total_out']
+del(data['tokens'][0]['total_out'])
 print(data)
 
+# Внутри "sec_token_info" изменить название ключа “price” на “total_price”.
+data.pop(data['tokens'][1]['sec_token_info']['price'])
+# print(data['tokens'][1]['sec_token_info']['price'])
+data
+#
 
-# В “ETH” добавить ключ “total_diff” со значением 100.
 #
-# Внутри “fst_token_info” значение ключа “name” поменять с “fdf” на “doge”.
-#
-# Удалить “total_out” из tokens и присвоить его значение в “total_out” внутри “ETH”.
+
 #
 # Внутри "sec_token_info" изменить название ключа “price” на “total_price”.
