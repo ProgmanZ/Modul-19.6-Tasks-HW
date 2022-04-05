964503e075c76d4ef6d743d7e56dde7ddcb28d3e
ProgmanZ <dumphill@gmail.com>
2022-04-05 17:31:27 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово. Task 04 - Готово.

--- Task-04.py
+++ Task-04.py
@@ -1,2 +1,53 @@
 # Задача 4. Товары.
 
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+
+def counter(dict_list):
+    sum_quantity = 0
+    sum_price = 0
+    for i in dict_list:
+        sum_quantity += i['quantity']
+        sum_price += i['price']
+    return sum_quantity, sum_price
+
+
+def word(number):
+    if number % 10 == 1 and number % 100 != 11:
+        return 'рубль'
+    elif number % 10 in [2, 3, 4]:
+        return 'рубля'
+    else:
+        return 'рублей'
+
+
+print('Результат работы программы:')
+for item in goods:
+
+    quontity, summ = counter(store[goods[item]])
+    print(f'{item} - {quontity}, стоимость {summ} {word(summ)}.')
+
