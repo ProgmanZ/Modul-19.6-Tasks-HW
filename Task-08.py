a2280cf8d356f9a6ad4a598ed6d8c682bf5b67c1
ProgmanZ <dumphill@gmail.com>
2022-04-14 19:30:51 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово. Task 04 - Готово. Task 05 - Готово. Task 06 - Готово. Task 07 - Готово. Task 08 - Готово. Task 08 test - Готово

--- Task-08.py
+++ Task-08.py
@@ -3,35 +3,60 @@
 import random
 
 number_user = int(input('Введите максимальное число: '))
-riddle = random.randint(1,number_user)
-flag = True
-set_yes = set()
-set_no = set()
 
-while flag:
+riddle = random.randint(1, number_user)  # число, которое загадал Артур
 
-    numbers_answer = input('Нужное число есть среди вот этих чисел: ')
-    if numbers_answer.isdigit():
-        numbers_set = set( int(i) for i in numbers_answer.split(' '))
-        if riddle in numbers_set:
-            print('Ответ Артёма: Да')
-            if not len(set_yes):
-                set_yes.update(numbers_set)
-            else:
-                set_yes.add(numbers_set)
-        else:
-            print('Ответ Артёма: Нет')
-            if not len(set_no):
-                set_no.update(numbers_set)
-            else:
-                set_yes.discard(set_no)
-    elif numbers_answer.lower() == 'помогите':
-        flag = False
+set_say_yes = {int(i) for i in range(1, number_user + 1)}  # сет с последовательностью чисел содержащий верный элемент
+
+set_say_no = set()  # сет с последовательностью чисел при отрицательном ответе
+
+
+def check_enter(enter_list):  # проверка ввода от пользователя
+    if enter_list == 'помогите':
+        return -1
+    elif not enter_list.isalpha():
+        return {int(i) for i in enter_list.split(' ')}
     else:
-         print('Неверный ввод данных. Проверьте вводимые данные. Если требуется помошь - введите "Помогите".')
-    # print(numbers_set)
+        return 0
 
 
+def print_numbers(set_user):
+    for elem in set_user:
+        print(elem, end=' ')
 
 
-print()
+while True:
+
+    user_numbers = check_enter(input('Нужное число есть среди вот этих чисел: '))
+
+    if user_numbers == 0:
+        # неверный ввод
+        print('Неверный ввод данных. Проверьте вводимые данные. Если требуется помошь - введите "Помогите".')
+        continue
+
+    elif user_numbers == -1:
+        # "помогите"
+        print('Артём мог загадать следующие числа: ', end='')
+        print_numbers(set_say_yes)
+        break
+
+    else:
+        if riddle in user_numbers:
+            print("Ответ Артёма: Да.")
+            set_say_yes.clear()
+            set_say_yes.update(user_numbers)
+            if len(set_say_yes) == 1:
+                print('Вы угадали число!')
+                break
+
+        else:
+            print("Ответ Артёма: Нет.")
+            for elem in user_numbers:
+                set_say_yes.discard(elem)
+                set_say_no.add(elem)
+            if len(set_say_yes) == 1:
+                print("Вы перебрали числа: ", end='')
+                print_numbers(set_say_no)
+                print("\nОсталось только одно число, которое вы не ввели. Это: ", end='')
+                print_numbers(set_say_yes)
+                break
