e4aaff3a4a0a908271cef0e738ad115f38a0cf0e
ProgmanZ <dumphill@gmail.com>
2022-04-14 19:34:55 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово. Task 04 - Готово. Task 05 - Готово. Task 06 - Готово. Task 07 - Готово. Task 08 - Готово. Task 08 test - Готово

--- Task-08.py
+++ Task-08.py
@@ -2,16 +2,8 @@
 
 import random
 
-number_user = int(input('Введите максимальное число: '))
 
-riddle = random.randint(1, number_user)  # число, которое загадал Артур
-
-set_say_yes = {int(i) for i in range(1, number_user + 1)}  # сет с последовательностью чисел содержащий верный элемент
-
-set_say_no = set()  # сет с последовательностью чисел при отрицательном ответе
-
-
-def check_enter(enter_list):  # проверка ввода от пользователя
+def check_enter(enter_list):
     if enter_list == 'помогите':
         return -1
     elif not enter_list.isalpha():
@@ -25,35 +17,45 @@ def print_numbers(set_user):
         print(elem, end=' ')
 
 
+number_user = int(input('Введите максимальное число: '))
+riddle = random.randint(1, number_user)
+set_say_yes = {int(i) for i in range(1, number_user + 1)}
+set_say_no = set()
+
 while True:
 
     user_numbers = check_enter(input('Нужное число есть среди вот этих чисел: '))
 
     if user_numbers == 0:
-        # неверный ввод
+
         print('Неверный ввод данных. Проверьте вводимые данные. Если требуется помошь - введите "Помогите".')
         continue
 
     elif user_numbers == -1:
-        # "помогите"
+
         print('Артём мог загадать следующие числа: ', end='')
         print_numbers(set_say_yes)
         break
 
     else:
         if riddle in user_numbers:
+
             print("Ответ Артёма: Да.")
             set_say_yes.clear()
             set_say_yes.update(user_numbers)
             if len(set_say_yes) == 1:
+
                 print('Вы угадали число!')
                 break
 
         else:
+
             print("Ответ Артёма: Нет.")
+
             for elem in user_numbers:
                 set_say_yes.discard(elem)
                 set_say_no.add(elem)
+
             if len(set_say_yes) == 1:
                 print("Вы перебрали числа: ", end='')
                 print_numbers(set_say_no)
